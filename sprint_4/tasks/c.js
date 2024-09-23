593caed7308a7bee1ac65f0f135b781388f95850
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2024-09-23 11:56:47 +0300

    Добавлена задача C sprint 4

---
+++ sprint_4/tasks/c.js
@@ -0,0 +1,29 @@
+const matchString = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  const uniqCharsSecondStr = Array.from(new Set(b));
+  const hashMapChars = Array.from(new Set(a)).reduce((acc, char, idx) => {
+    const anotherStrChar = uniqCharsSecondStr[idx];
+
+    if (!acc.has(char)) {
+      acc.set(char, anotherStrChar);
+    }
+
+    return acc;
+  }, new Map());
+
+  for (let i = 0; i < a.length; i++) {
+    const charFirst = a[i];
+    const charSecond = b[i];
+
+    if (charSecond !== hashMapChars.get(charFirst)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(matchString("agg", "bcc"));
